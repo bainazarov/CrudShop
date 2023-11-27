aa6dadaeb6ce3ca3e0f8a5c9a4af9e3aa4f38182
Abdurakhmon Bainazarov <baynazarowv@gmail.com>
2023-11-27 13:44:38 +0400

    refactoring article migration

--- src/main/java/com/crudshop/demo/service/ProductService.java
+++ src/main/java/com/crudshop/demo/service/ProductService.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.UUID;
 
 @Component
